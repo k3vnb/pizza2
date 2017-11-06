4a51bf592952f4eb2aa745718b224d47825f8387
Kevin Boyle <papershack@gmail.com>
2017-11-05 23:03:33 -0800

    add new 'page' for confirmed order

---
+++ README.md
@@ -0,0 +1,55 @@
+# _Pizzarrghh_
+
+#### _Pizzarrghh is designed to help you order a custom pizza, {11/3/2017}_
+
+#### By _**Kevin Boyle**_
+
+## Description
+
+_Have you ever wanted to order a pizza? Pizzarrghh is a website that helps you practice ordering a pizza. Upon selecting a size, you are invited to select from regular & specialty toppings. You can see your order price calculated as you process your order, and you can change your order size & toppings to see the price change accordingly _
+
+## Setup/Installation Requirements
+
+* _Go on to the page at https://lemurriot.github.io/pizza/_
+* _If you are using a modern browser the page will display_
+
+
+## Specifications
+
+* _It can allow user to select a single pizza size_
+  * _Example Input: select large from drop-down menu, click "Select Size" button_
+  * _Example Output: display(Your size selection: "large", Total Price: $12.00)_
+* _It can allow user to select a "cheap" topping_
+  * _Example Input: in Checkbox form: select: Pepperoni; click "Add Toppings" button_
+  * _Example Output: display adds $2 to total price (base price is established by pizza size). Display says: your size selection: large, Total Price $14.00_
+* _It can allow user to select an "expensive" topping_
+  * _Example Input: in Checkbox form: select: Fresh Ricotta; click "Add Toppings" button_
+  * _Example Output: display adds $4 total price (base price is established by pizza size). Display says:your size selection: large, Total Price $16.00)_
+* _It can allow user to select multiple "expensive" and "cheap" toppings by clicking multiple checkboxes_
+  * _Example Input: in Checkbox form: select: Pepperoni, Onions, Fresh Ricotta, Calamata Olives; click "Add Toppings" button_
+  * _Example Output: display adds $4 per expensive topping and $2 per cheap topping (base price is established by pizza size). Display says: your size selection: "large"; your total price $24.00_
+* _It can allow user to change pizza size by re-selecting from drop-down menu and clicking "Select Size" button_
+  * _Example Input: in dropdown menu: change pizza size, click "Select Size"_
+  * _Example Output: display changes base price of pizza to corresponding price, re-calculates total accordingly_
+* _It can allow user to change topping selections by changing selected checkboxes and clicking "Add Toppings" again_
+  * _Example Input: in Checkbox menu: change selection choice, click "Add Toppings"_
+  * _Example Output: display re-calculates total accordingly_
+
+
+## Known Bugs
+
+_ No known bugs_.
+
+## Support and contact details
+
+_Please contact Kevin Boyle at papershack@gmail.com with questions or comments_
+
+## Technologies Used
+
+_HTML, CSS, including Bootstrap, and JavaScript including jQuery were used to construct this page. It should function in any browser_
+
+### License
+
+*Creative Commons (CCPL) License*
+
+Copyright (c) 2017 **_Kevin Boyle_**
---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+//business logic begins
+
+function Pizza(size, cheapToppings, expensiveToppings) {
+  this.size = size;
+  this.cheapToppings = cheapToppings;
+  this.expensiveToppings = expensiveToppings;
+};
+
+Pizza.prototype.pizzaPrice = function() {
+  var pizzaPrice = 10;
+  if (this.size == "small") {
+    pizzaPrice = pizzaPrice - 1;
+  } else if (this.size == "large") {
+    pizzaPrice = pizzaPrice + 2;
+  }
+  if (this.cheapToppings.length >= 1) {
+    pizzaPrice = pizzaPrice + (this.cheapToppings.length * 2)
+  }
+  if (this.expensiveToppings.length >= 1) {
+    pizzaPrice = pizzaPrice + (this.expensiveToppings.length * 4)
+  } return pizzaPrice;
+};
+
+
+
+//end of Business Logic
+$(document).ready(function() {
+  $("#size-add").click(function (event) {
+    var pizzaSize = $("select#size-select").val();
+    $("#show-size").empty();
+    $(".toppings").show();
+    $(".your-size").show();
+    $("#show-size").append(pizzaSize);
+  });
+  $("form#pizzaform").submit(function (event){
+    event.preventDefault();
+    $("#your-order").empty();
+    $(".display-result").show();
+    $("#confirm").show();
+    var pizzaSize = $("select#size-select").val();
+
+    var cheapToppings = [];
+      $("input:checkbox[name=cheap-topping]:checked").each(function() {
+      var allCheapToppings = $(this).val();
+      cheapToppings.push(allCheapToppings);
+    });//end of 'add'' cheap toppings select
+      console.log(cheapToppings);
+
+    var expensiveToppings = [];
+    $("input:checkbox[name=expensive-topping]:checked").each(function() {
+    var allExpensiveToppings = $(this).val();
+    expensiveToppings.push(allExpensiveToppings);
+    });//end of 'add' exp toppings select
+      console.log(expensiveToppings);
+
+    var yourPizza = new Pizza(pizzaSize, cheapToppings, expensiveToppings);
+
+    $("#your-order").append("$" + yourPizza.pizzaPrice() + ".00");
+    console.log(yourPizza.pizzaPrice());
+    $("#confirm").click(function() {
+      if ((cheapToppings.length != 0) && (expensiveToppings.length != 0)) {
+        $("#your-confirmed-order").append("One " + pizzaSize + " pizza topped with " + cheapToppings + " &" + expensiveToppings + ". Total Price: $" + yourPizza.pizzaPrice() + ".00 </br> Thank you for your Order!");
+      } else if ((cheapToppings.length != 0) && (expensiveToppings.length == 0)) {
+        $("#your-confirmed-order").append("One " + pizzaSize + " pizza topped with " + cheapToppings + ". Total Price: $" + yourPizza.pizzaPrice() + ".00 </br> Thank you for your Order!");
+    }    else if ((cheapToppings.length == 0) && (expensiveToppings.length != 0)) {
+        $("#your-confirmed-order").append("One " + pizzaSize + " pizza topped with " +  expensiveToppings + ". Total Price: $" + yourPizza.pizzaPrice() + ".00 </br> Thank you for your Order!");
+    } else  {
+        $("#your-confirmed-order").append("One " + pizzaSize + " pizza. Total Price: $" + yourPizza.pizzaPrice() + ".00 </br> Thank you for your Order!");
+      }
+      $(".prepare-order").hide();
+      $(".confirm-order").show();
+    });
+  }); //end of submit
+});//end of doc ready
